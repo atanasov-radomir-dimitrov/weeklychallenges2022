88facdbd7a77aa0b03a1ab2f16102d67836d1b84
atanasov-radomir-dimitrov <radomirdimitrovatanasov@gmail.com>
2022-02-12 11:36:01 +0100

    Week 6 challenge

---
+++ app/src/main/java/com/radomiratanasov/weeklychallenge2022/week6.kt
@@ -0,0 +1,37 @@
+package com.radomiratanasov.weeklychallenge2022
+
+import java.lang.StringBuilder
+
+/*
+ * Reto #6
+ * INVIRTIENDO CADENAS
+ * Fecha publicación enunciado: 07/02/22
+ * Fecha publicación resolución: 14/02/22
+ * Dificultad: FÁCIL
+ *
+ * Enunciado: Crea un programa que invierta el orden de una cadena de texto sin usar funciones
+ * propias del lenguaje que lo hagan de forma automática.
+ * - Si le pasamos "Hola mundo" nos retornaría "odnum aloH"
+ *
+ * Información adicional:
+ * - Usa el canal de nuestro discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar ayuda la acomunidad.
+ * - Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.
+ * - Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
+ * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
+ *
+ */
+fun main() {
+
+    val s1 = "Hola mundo!"
+    val chars1 = s1.toCharArray()
+
+    val chars2 = CharArray(chars1.size)
+
+    for ((i, element) in chars1.withIndex()) {
+        chars2[chars1.size - 1 - i] = element
+    }
+
+    println(s1)
+    print(String(chars2))
+
+}
